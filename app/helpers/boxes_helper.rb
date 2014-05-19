1c40d5a373699fe39c63a2ee387a9c42470ffe78
Constantin Hofstetter <Consti@Consti.de>
2014-05-19 14:53:26 +0200

    don't use nested forms, use a textarea

--- app/helpers/boxes_helper.rb
+++ app/helpers/boxes_helper.rb
@@ -1,10 +1,2 @@
 module BoxesHelper
-
-  def build_box(box)
-    10.times do
-      box.items.build
-    end
-    box
-  end
-
 end
--- app/models/box.rb
+++ app/models/box.rb
@@ -2,4 +2,22 @@ class Box < ActiveRecord::Base
   has_many :items
 
   accepts_nested_attributes_for :items, allow_destroy: true, reject_if: :all_blank
+
+  attr_accessor :items_list
+
+  before_save :split_items_list
+
+  def items_as_list
+    self.items.map(&:name).join(", ")
+  end
+
+  def split_items_list
+    if items_list.present?
+      self.items = []
+      items_list.split(',').each do |item|
+        self.items << Item.where(name: item).first_or_create
+      end
+    end
+  end
+
 end
